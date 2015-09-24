07819b579b3e099a930569c97fba2e8786174a27
Juan Manuel <jmruiz.dev@gmail.com>
2015-09-24 14:07:01 -0500

    Add output message to MyLIbrary1

--- MyLibrary1/mylibrary1.h
+++ MyLibrary1/mylibrary1.h
@@ -1,6 +1,7 @@
 #ifndef MYLIBRARY1_H
 #define MYLIBRARY1_H
 
+#include <QString>
 #include "mylibrary1_global.h"
 
 class MYLIBRARY1SHARED_EXPORT MyLibrary1
@@ -8,6 +9,7 @@ class MYLIBRARY1SHARED_EXPORT MyLibrary1
 
 public:
     MyLibrary1();
+    QString output;
 };
 
 #endif // MYLIBRARY1_H
