2f2e13c7f5c72d561377a42a1ea7932a1b32febe
Juan Manuel <jmruiz.dev@gmail.com>
2015-09-24 17:06:49 -0500

    Intent to show a GUI from a DLL
    Here I try to show a QDialog like another qDialog... But... I can't
    do it

--- MainApp/mainwindow.cpp
+++ MainApp/mainwindow.cpp
@@ -16,3 +16,10 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_actionMyLibrary2_triggered()
+{
+    // This is the point where I want show the QDialog from MyLibrary2
+    Library2 = new MyLibrary2(); // This shot a permission error
+    Library2->show();
+}
--- MainApp/mainwindow.h
+++ MainApp/mainwindow.h
@@ -5,6 +5,9 @@
 #include <QDebug>
 #include "../MyLibrary1/mylibrary1.h"
 
+// The include and declaration don't shot any problem
+#include "../MyLibrary2/mylibrary2.h"
+
 namespace Ui {
 class MainWindow;
 }
@@ -17,9 +20,14 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_actionMyLibrary2_triggered();
+
 private:
     Ui::MainWindow *ui;
     MyLibrary1 *Library1;
+    // The include and declaration don't shot any problem
+    MyLibrary2 *Library2;
 };
 
 #endif // MAINWINDOW_H
